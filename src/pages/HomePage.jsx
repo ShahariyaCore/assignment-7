4c6dfa91ad05e06b88ffd0050f427281a6756023
nishat822 <shahariya.jaman00@gmail.com>
2026-04-15 01:24:08 +0600

    feat: add FriendCard component with status badges, tags, and contact button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import FriendsDashboard from '../components/FriendsDashboard'
 
 const HomePage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-8">Welcome to Assignment 7</h1>
       <p className="text-lg text-center">This is the home page of your React application.</p>
-      
+      <FriendsDashboard></FriendsDashboard>
     </div>
   )
 }
