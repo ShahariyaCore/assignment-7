fd15a06a70aaafc324f5dedb174c86d6d8c922b2
nishat822 <shahariya.jaman00@gmail.com>
2026-04-16 04:45:49 +0600

    fix: adjust router basename to work in both localhost and GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import RootLayout from "./layout/RootLayout";
 import HomePage from "./pages/HomePage";
-import FriendDetails from "./components/FriendDetails"; // ✅ import new details page
-import Timeline from "./pages/Timeline"; // ✅ import Timeline component
-import Stats from "./pages/Stats"; // ✅ import Stats component
-import NotFound from "./components/NotFound"; // ✅ import 404 page
+import FriendDetails from "./components/FriendDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./components/NotFound";
 
-// ✅ Import toastify styles and container
+// Toastify
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "friends/:id", element: <FriendDetails /> },
+        { path: "timeline", element: <Timeline /> },
+        { path: "stats", element: <Stats /> },
+        { path: "*", element: <NotFound /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "/friends/:id", // dynamic route for friend details
-        element: <FriendDetails />,
-      },
-      { path: "/timeline", element: <Timeline /> }, 
-      { path: "/stats", element: <Stats /> }, // ✅ dynamic route for friend details
-      { path: "*", element: <NotFound /> },
-      
-    ],
-  },
-]);
+   basename: import.meta.env.MODE === "production" ? "/assignment-7" : "/", 
+    // ✅ use repo name only in production
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-    {/* ✅ ToastContainer at root */}
     <ToastContainer position="top-right" autoClose={3000} />
   </StrictMode>
 );
